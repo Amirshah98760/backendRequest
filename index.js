dbffd0a97043cee4d181f3879abe43e2fd59a7fc
Amirshah98760 <amirshah98760@gmail.com>
2024-09-20 20:44:46 +0500

    post request is added

--- index.js
+++ index.js
@@ -7,7 +7,20 @@ app.use(express.json());
 
 let items = [];
 
+//get request 
 app.get('/items', (req, res)=> { 
     res.json(items);
 });
+
+// Post
+app.post('/items', (req, res) => {
+    const newItem = {
+        id: items.length + 1,
+        name: req.body.name,
+    };
+    items.push(newItem);
+    res.status(201).json(newItem);
+});
+
+
 
